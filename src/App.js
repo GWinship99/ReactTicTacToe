5227233e7aba1c6292f3014d076d5c73642a7e13
GWinship99 <60112079+GWinship99@users.noreply.github.com>
2025-11-24 10:03:31 +0000

    code refactoring

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ function Square({value, onSquareClick}) {
     return <button className="square" onClick={onSquareClick}>{value}</button>
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares,setSquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if(squares[i] || calculateWinner(squares)) {
       return;
@@ -18,11 +15,9 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-   
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+   onPlay(nextSquares);
   }
-  
+
    const winner = calculateWinner(squares);
   let status;
   if(winner) {
@@ -50,6 +45,29 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
+  );
+}
+
+/* TODO: Add history of moves to game */
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
